a802e7a248cf9edf35e1f0d93881839459c1927c
jack <jackvwh@hotmail.com>
2024-03-02 09:27:19 +0100

    navLink done

--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -6,20 +6,16 @@ export default function NavHeader() {
     <nav>
       <ul>
         <li>
-          <a href="/">Home</a>
-          {/* <Link to="/">Home</Link> */}
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <a href="/categories">Categories</a>
-          {/* <Link to="/categories">Categories</Link> */}
+          <NavLink to="/categories">Categories</NavLink>
         </li>
         <li>
-          <a href="/recipes">Recipes</a>
-          {/* <Link to="/recipes">Recipes</Link> */}
+          <NavLink to="/recipes">Recipes</NavLink>
         </li>
         <li>
-          <a href="/add">Add</a>
-          {/* <Link to="/add">Add</Link> */}
+          <NavLink to="/add">Add</NavLink>
         </li>
         <AuthStatus />
       </ul>
