8c73d5d08731f9cdb00afefc45d62e5839ef29bc
jack <jackvwh@hotmail.com>
2024-03-04 11:37:37 +0100

    protect routes and hiding links

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Home from "./Home";
 import "./App.css";
 import RecipesLayout from "./recipes/RecipesLayout";
 import Logout from "./security/Logout";
+import RequireAuth from "./security/RequireAuth";
 
 export default function App() {
   //const auth = useAuth();
@@ -21,7 +22,14 @@ export default function App() {
           <Route path=":id" element={<Recipe />} />
           <Route path="test" element={<h1>Test</h1>} />
         </Route>
-        <Route path="/add" element={<RecipeForm />} />
+        <Route
+          path="/add"
+          element={
+            <RequireAuth roles={["ADMIN"]}>
+              <RecipeForm />
+            </RequireAuth>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<h2>Not Found</h2>} />
--- src/NavHeader.tsx
+++ src/NavHeader.tsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
 import AuthStatus from "./security/AuthStatus";
+import { useAuth } from "./security/AuthProvider";
 
 export default function NavHeader() {
+  const auth = useAuth();
   return (
     <nav>
       <ul>
@@ -14,9 +16,12 @@ export default function NavHeader() {
         <li>
           <NavLink to="/recipes">Recipes</NavLink>
         </li>
-        <li>
-          <NavLink to="/add">Add</NavLink>
-        </li>
+        {auth.isLoggedInAs(["ADMIN"]) && (
+          <li>
+            <NavLink to="/add">Add</NavLink>
+          </li>
+        )}
+
         <li>
           <NavLink to="/contact">Contact</NavLink>
         </li>
