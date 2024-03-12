5e989e4721ea749230cdb83a9a248f3a05a5a11d
jack <jackvwh@hotmail.com>
2024-03-12 10:53:09 +0100

    nested recipe add and added category add

--- src/App.tsx
+++ src/App.tsx
@@ -17,19 +17,23 @@ export default function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/categories/" element={<Categories />} />
+
+        <Route path="/categories" element={<Categories />}>
+          <Route path="add" element={<h1>Add a category</h1>} />
+        </Route>
+
         <Route path="/recipes" element={<RecipesLayout />}>
           <Route path=":id" element={<Recipe />} />
-          <Route path="test" element={<h1>Test</h1>} />
+          <Route
+            path="add"
+            element={
+              <RequireAuth roles={["USER", "ADMIN"]}>
+                <RecipeForm />
+              </RequireAuth>
+            }
+          />
         </Route>
-        <Route
-          path="/add"
-          element={
-            <RequireAuth roles={["ADMIN"]}>
-              <RecipeForm />
-            </RequireAuth>
-          }
-        />
+
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="*" element={<h2>Not Found</h2>} />
